a95985bdd9975ad75b418d2eb51cdaa207abdccc
chiranthcs06 <chiranthcs5@gmail.com>
2024-11-23 21:29:56 +0530

    first

---
+++ hello_world/target/debug/deps/hello_world-51fc876a2b2d2e87.d
@@ -0,0 +1,5 @@
+/home/chiranthcs/RUST_PROJECT/Rust/hello_world/target/debug/deps/libhello_world-51fc876a2b2d2e87.rmeta: src/main.rs
+
+/home/chiranthcs/RUST_PROJECT/Rust/hello_world/target/debug/deps/hello_world-51fc876a2b2d2e87.d: src/main.rs
+
+src/main.rs:
