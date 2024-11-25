aaac3becb23b038698675b16bdfdaa03e4a21de2
Chiranthcs5@gmail.com <Chiranthcs5@gmail.com>
2024-11-25 12:58:11 +0530

    new program

---
+++ hello_world/src/bin/program2.rs
@@ -0,0 +1,17 @@
+fn main(){
+    println!("{} months",12);
+
+
+    println!("{0} is man, and {1} is female","anuroop","tarun");
+
+    pritnln!("Base 10: {}",420);
+    println!("Base 2(binary): {:b}",420);
+    println!("Base 8(octal): {:o}",420);
+    println!("Base 16(hexadecimal: {:x}",420);
+
+    println!("{num:>5",num=1);
+    println!("{num:0<5}",num=1);
+
+
+    println!("This is {0}{1}. The legned {1}","James","Bond");
+}
